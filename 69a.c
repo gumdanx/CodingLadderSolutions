8b0f0bb870328571a44802a0c6f340ca43216474
Tiago Peres Moura Fe <31408208+gumdanx@users.noreply.github.com>
2023-10-24 13:47:33 -0300

    Update 69a.c
    Translated to simplified english

--- 69a.c
+++ 69a.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
  
-void sim()
+void vectorialSum(int j)
 {
-	printf("YES\n");
-}
- 
-void nao()
-{
-	printf("NO\n");
-}
- 
-void somaVet(int j)
-{
-	int i;
-	int x[2], y[2], z[2];
+	int i; // counter
+	int x[2], y[2], z[2]; // [0] as origin, [1] as 'new point'
 	
-	scanf("%d%d%d", &x[0], &y[0], &z[0]);
-	for(i = 1; i < j; i++)
+	scanf("%d%d%d", &x[0], &y[0], &z[0]); // origin
+	for(i = 1; i < j; i++) // each new point add to origin
 	{
 		scanf("%d%d%d", &x[1], &y[1], &z[1]);
 	
@@ -24,20 +14,21 @@ void somaVet(int j)
 		y[0] += y[1];
 		z[0] += z[1];
 	}
-	if(x[0] == 0 && y[0] == 0 && z[0] == 0)
+	
+	if(x[0] == 0 && y[0] == 0 && z[0] == 0) // equilibrium
 	{
-		sim();
+		printf("YES");
 	}
 	else
 	{
-		nao();
+		printf("NO");
 	}
 }
  
 int main()
 {
-	int n; // numero de linhas
+	int n; // number of lines
 	scanf("%d", &n);
-	somaVet(n);
+	vectorialSum(n);
 	return 0;
 }
